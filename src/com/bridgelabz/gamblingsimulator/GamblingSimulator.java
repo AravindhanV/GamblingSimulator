b868705db61ea42d2d21641f5ec22268d2b0df56
Aravindhan V <aravindhan.v@perfios.com>
2021-08-24 08:46:01 +0530

    ADDED: Logic for bet loss and win

--- src/com/bridgelabz/gamblingsimulator/GamblingSimulator.java
+++ src/com/bridgelabz/gamblingsimulator/GamblingSimulator.java
@@ -3,7 +3,23 @@ package com.bridgelabz.gamblingsimulator;
 public class GamblingSimulator {
 	static final int DAILY_STAKE = 100;
 	static final int BET_EVERY_GAME = 1;
+	static final int LOSE = 0;
+	static final int WIN = 1;
 	
 	public static void main(String[] args) {
+		int currentBalance = DAILY_STAKE;
+		currentBalance = simulateBet(currentBalance);
+	}
+	
+	public static int simulateBet(int currentBalance) {
+		int randomNumber = (int)Math.floor(Math.random() * 2);
+		
+		if(randomNumber == 0) {
+			currentBalance--;
+		} else {
+			currentBalance++;
+		}
+		
+		return currentBalance;
 	}
 }
