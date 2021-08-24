d1e09bee41e7e7a07e6b22a74015bc1e552c0a6c
Aravindhan V <aravindhan.v@perfios.com>
2021-08-24 08:34:22 +0530

    REFACTOR: Parameters are made global constants

--- src/com/bridgelabz/gamblingsimulator/GamblingSimulator.java
+++ src/com/bridgelabz/gamblingsimulator/GamblingSimulator.java
@@ -1,8 +1,9 @@
 package com.bridgelabz.gamblingsimulator;
 
 public class GamblingSimulator {
+	static final int DAILY_STAKE = 100;
+	static final int BET_EVERY_GAME = 1;
+	
 	public static void main(String[] args) {
-		int dailyStake = 100;
-		int betEveryGame = 1;
 	}
 }
