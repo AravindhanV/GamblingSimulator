64b91a75578743ba8c97c4f9817d07608c6344b8
Aravindhan V <aravindhan.v@perfios.com>
2021-08-24 09:55:13 +0530

    ADDED: Simulation over multiple days

--- src/com/bridgelabz/gamblingsimulator/GamblingSimulator.java
+++ src/com/bridgelabz/gamblingsimulator/GamblingSimulator.java
@@ -5,12 +5,24 @@ public class GamblingSimulator {
 	static final int BET_EVERY_GAME = 1;
 	static final int LOSE = 0;
 	static final int WIN = 1;
+	static final int NO_OF_DAYS = 20;
 
 	public static void main(String[] args) {
-		int currentBalance = DAILY_STAKE;
-		while (currentBalance > 0.5 * DAILY_STAKE && currentBalance < 1.5 * DAILY_STAKE) {
-			currentBalance = play(currentBalance);
+		int currentBalance = 0;
+		for(int day=0;day<NO_OF_DAYS;day++) {
+			int dailyAmount = DAILY_STAKE;
+			while (dailyAmount > 0.5 * DAILY_STAKE && dailyAmount < 1.5 * DAILY_STAKE) {
+				dailyAmount = play(dailyAmount);
+			}
+//			System.out.println(dailyAmount);
+			currentBalance += dailyAmount;
 		}
+		calculateGainOrLoss(currentBalance);
+	}
+	
+	public static void calculateGainOrLoss(int currentBalance) {
+		int netChange = currentBalance-(NO_OF_DAYS * DAILY_STAKE);
+		System.out.println("Total amount won or lost = $"+(netChange>0?netChange:-netChange)+" "+(netChange>0?"Gained":"Lost"));
 	}
 
 	public static int play(int currentBalance) {
