ed2880baee8393a7e0feeaadbefafd15b3a697a5
Aravindhan V <aravindhan.v@perfios.com>
2021-08-24 09:10:42 +0530

    ADDED: Stopping criteria for gambling defined

--- src/com/bridgelabz/gamblingsimulator/GamblingSimulator.java
+++ src/com/bridgelabz/gamblingsimulator/GamblingSimulator.java
@@ -5,21 +5,23 @@ public class GamblingSimulator {
 	static final int BET_EVERY_GAME = 1;
 	static final int LOSE = 0;
 	static final int WIN = 1;
-	
+
 	public static void main(String[] args) {
 		int currentBalance = DAILY_STAKE;
-		currentBalance = play(currentBalance);
+		while (currentBalance > 0.5 * DAILY_STAKE && currentBalance < 1.5 * DAILY_STAKE) {
+			currentBalance = play(currentBalance);
+		}
 	}
-	
+
 	public static int play(int currentBalance) {
-		int randomNumber = (int)Math.floor(Math.random() * 2);
-		
-		if(randomNumber == 0) {
+		int randomNumber = (int) Math.floor(Math.random() * 2);
+
+		if (randomNumber == 0) {
 			currentBalance--;
 		} else {
 			currentBalance++;
 		}
-		
+
 		return currentBalance;
 	}
 }
